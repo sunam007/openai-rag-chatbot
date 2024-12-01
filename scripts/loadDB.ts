43af39489e3e2ccea14fc0cac7aba3e5943e2a0a
sunam007 <asaduzzaman.sunam@gmail.com>
2024-12-01 23:15:38 +0600

    project initiated

---
+++ scripts/loadDB.ts
@@ -0,0 +1,28 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import OpenAI from "openai";
+import "dotenv/config";
+
+const {
+  ASTRA_DB_NAMESPACE,
+  ASTRA_DB_COLLECTION,
+  ASTRA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN,
+  OPEN_AI_API_KEY,
+} = process.env;
+
+const openai = new OpenAI({ apiKey: OPEN_AI_API_KEY });
+
+const f1Data = [
+  "https://en.wikipedia.org/wiki/Formula_One",
+  "https://www.formula1.com/en/latest",
+];
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN);
+const db = client.db(ASTRA_DB_API_ENDPOINT, { keyspace: ASTRA_DB_NAMESPACE });
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 100,
+});
